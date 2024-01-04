c7184fd7eda94bea363309adda37f3b53733dc52
Quienten <quientenmiller2@gmail.com>
2024-01-04 08:26:40 -0800

    Add some about information. Add SelfReferenceGag.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ProjectTile from "./components/ProjectTile.jsx";
 import ExperienceTile from "./components/ExperienceTile.jsx";
 import SocialButton from "./components/SocialButton.jsx"
 import SideNav from "./components/SideNav.jsx";
+import SelfReferenceGag from "./components/SelfReferenceGag.jsx"
 
 function App() {
 
@@ -34,8 +35,8 @@ function App() {
               </header>
 
               <main className="pt-24 lg:w-7/12 lg:py-24">
-                  <section id="about">
-                      <p></p>
+                  <section id="about" className="mb-16 lg:mb-24">
+                      <p>{"My name is Quienten, I'm a student at the University of Washington Tacoma. Passionate about developing cutting-edge software. I spend my day expanding my expanding my computer science knowledge and communicating ideas to my peers."}</p>
                       <p></p>
                       <p></p>
                   </section>
@@ -43,7 +44,12 @@ function App() {
                   <section id="projects">
                     <ol>
                         {projects.map((project, i) => {
-                            return <ProjectTile key={i} project={project}/>
+                            if(project.name === "Portfolio Website") {
+                                return <SelfReferenceGag key={i} project={project}/>
+                            } else {
+                                return <ProjectTile key={i} project={project}/>
+                            }
+
                         })}
                     </ol>
                   </section>
@@ -56,7 +62,7 @@ function App() {
                       </ol>
                   </section>
 
-                  <section id="contacts">
+                  <section id="contact">
 
                   </section>
               </main>
